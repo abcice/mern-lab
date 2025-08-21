1001dd6f678543ca1835c16083aceaff6a0a2681
Mohamed Meshaima <mohamedoaderu@gmail.com>
2025-08-21 16:57:23 +0300

    created routes and finished

--- hoot/controllers/api/hoots.js
+++ hoot/controllers/api/hoots.js
@@ -5,13 +5,23 @@ export {
 };
 async function index(req, res) {
     try{
-        const hoots = await Hoot.find({})
+        const hoots = await Hoot.find({}).sort('-createdAt').populate('user', 'name');
+        res.status(200).json(hoots);
     } catch (e){
         res.status(400).json({ msg: e.message });
     }
     
 }
-async function create(req, res, next) {
-    try{} catch{}
+async function create(req, res) {
+    try{
+            req.body.user = req.user._id;
+            const hoot = await Hoot.create(req.body);
+            const populatedHoot = await hoot.populate('user', 'name');
+            res.status(201).json(populatedHoot);
+
+    } catch (e) {
+            res.status(400).json({ msg: e.message });
+
+    }
 
 }
